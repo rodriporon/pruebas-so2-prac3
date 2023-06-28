8e15f3001eef41af4750465316e64fe7dcf9cd36
Rodrigo Porón <3065713820401@ingenieria.usac.edu.gt>
2023-06-28 16:36:43 -0600

    s

--- Api/main.go
+++ Api/main.go
@@ -226,7 +226,7 @@ func parseSmapsData(smapsData string) (residentSize, virtualSize int, ramUsagePe
 		}
 
 		if i == len(blocks)-2 {
-			value := strings.Split(line[0], "-")
+			value := strings.Split(line[0], " ")
 			fmt.Println("---------------------")
 			fmt.Println("Posicion: ", i)
 			fmt.Println(value)
